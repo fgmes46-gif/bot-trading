b2ab68def99be740f1816c3c574d0fddb6103733
Cloud_Arquiteto_01 <fgmes46@gmail.com>
2026-03-25 19:56:37 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -107,6 +107,13 @@ def sinal():
 
     d = request.json
 
+    @app.route('/sinal', methods=['POST'])
+def receber_sinal():
+    data = request.json
+    
+    print("🔥 SINAL RECEBIDO:", data)  # 👈 ADICIONA ISSO
+
+
     # limite diário
     if sinais_hoje() >= MAX_SINAIS_DIA:
         return {"ok": False, "msg": "limite diário"}
